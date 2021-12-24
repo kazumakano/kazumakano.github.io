9c38dd76add7f3b3d00c0f0207f1001cf0db7420
kazumakano <kazumakano@outlook.jp>
2021-12-24 20:09:23 +0900

    minor modify

--- components/text-box.tsx
+++ components/text-box.tsx
@@ -11,7 +11,7 @@ type TextBoxProps = {
 export default function TextBox({ children, enableMargins, proportion }: TextBoxProps): JSX.Element {
   return (
     <div className="text-box" style={getBlockStyle(enableMargins, proportion)}>
-      <div className="text">
+      <div>
         {children}
       </div>
     </div>
