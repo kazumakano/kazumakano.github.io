ae78eb2c1573250eb874f0aeb3e54dc1dad73010
kazumakano <kazumakano@outlook.jp>
2021-12-25 16:14:16 +0900

    update publication

--- pages/publication.tsx
+++ pages/publication.tsx
@@ -18,32 +18,29 @@ export default function Publication({ pageIndex, transDirect, setPage }: Compone
   return (
     <Layout pageIndex={pageIndex} transDirect={transDirect} setPage={setPage} title="publication">
       <TextBox enableMargins={[false, false]} proportion={100}>
-      <h2>international</h2>
-      <Table hover striped>
-        <thead>
-          <tr>
-            <th>date</th>
-            <th>title</th>
-            <th></th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {papers.map((v, i) => {
-            return (
-              <tr key={i}>
-                <td>{dateTimeFormat.format(new Date(v.date[0], v.date[1] - 1))}</td>
-                <td>{v.title}</td>
-                <td><CopyBtn text={v.title} /></td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </Table>
-      <p>
-        This is publication page. This is publication page. This is publication page. This is publication page. This is publication page.<br />
-        This is publication page.
-      </p>
+        <h2>international</h2>
+        <Table hover striped>
+          <thead>
+            <tr>
+              <th>date</th>
+              <th>title</th>
+              <th>conference</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {papers.map((v, i) => {
+              return (
+                <tr key={i}>
+                  <td>{dateTimeFormat.format(new Date(v.date[0], v.date[1] - 1))}</td>
+                  <td>{v.title}</td>
+                  <td>{v.conference}</td>
+                  <td><CopyBtn text={v.title} /></td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </Table>
       </TextBox>
     </Layout>
   )
