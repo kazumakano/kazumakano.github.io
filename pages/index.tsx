a0f00ab90e9ae07819c984eb80c2e2b27509888b
kazumakano <kazumakano@outlook.jp>
2021-12-25 15:12:06 +0900

    update education

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,20 +11,25 @@ export default function Home({ pageIndex, transDirect, setPage }: ComponentProps
   return (
     <Layout pageIndex={pageIndex} transDirect={transDirect} setPage={setPage} title="about me">
       <TextBox enableMargins={[false, true]} proportion={70}>
-        <h2>history</h2>
+        <h2>education</h2>
         <Table borderless id={styles.eduTable}>
-          <tbody>
-            {history.map((v, i) => {
-              return (
-                <tr key={i}>
-                  <td>{v.year}</td>
-                  <td>{v.desc}</td>
+          {history.map((v, i) => {
+            return (
+              <tbody key={i}>
+                <tr>
+                  <td rowSpan={2}>{v.year[0]}</td>
+                  <td rowSpan={2}>~</td>
+                  <td rowSpan={2}>{v.year[1]}</td>
+                  <td>{v.department}</td>
                 </tr>
-              )
-            })}
-          </tbody>
+                <tr>
+                  <td>{v.school}</td>
+                </tr>
+              </tbody>
+            )
+          })}
         </Table>
-        
+
         <h2>experience</h2>
       </TextBox>
 
