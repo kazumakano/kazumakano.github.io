36b9228e51cd3bc44c7ec90ad2433bdd9743f123
kazumakano <kazumakano@outlook.jp>
2021-12-26 17:39:45 +0900

    minor modify

--- pages/skill.tsx
+++ pages/skill.tsx
@@ -62,30 +62,30 @@ const PROGRESS_BAR_CONFS: ProgressBarConf[] = [
   }
 ]
 
-const useLangs = () => {
-  let [langs, setLangs] = useState<LangDict | null>(null)
+const useLangDict = () => {
+  let [langDict, setLangDict] = useState<LangDict | null>(null)
   
-  const getLangs = useCallback(async () => {
-    langs =  await listAllReposLangs(process.env.NEXT_PUBLIC_USER_NAME!, process.env.NEXT_PUBLIC_TOKEN!)
-    setLangs(langs)
-    console.log(langs)
+  const getLangDict = useCallback(async () => {
+    langDict =  await listAllReposLangs(process.env.NEXT_PUBLIC_USER_NAME!, process.env.NEXT_PUBLIC_TOKEN!)
+    setLangDict(langDict)
+    console.log(langDict)
   }, [])
 
   useEffect(() => {
-    getLangs()
+    getLangDict()
   }, [])
 
-  return langs
+  return langDict
 }
 
 type ProgressBarsProps = {
-  langs: LangDict | null
+  langDict: LangDict | null
 }
 
 const calcProportion = (bytes: number) => 10 * Math.log(bytes)
 
-const ProgressBars = ({ langs }: ProgressBarsProps) => {
-  if (langs == null) {
+const ProgressBars = ({ langDict }: ProgressBarsProps) => {
+  if (langDict == null) {
     return (
       <Spinner animation="border" variant="dark" className={styles.spinner} />
     )
@@ -102,10 +102,10 @@ const ProgressBars = ({ langs }: ProgressBarsProps) => {
                   className="progress-bar"
                   style={{
                     backgroundColor: c.colors[l],
-                    width: calcProportion(l in langs ? langs[l] : 0)
+                    width: calcProportion(l in langDict ? langDict[l] : 0)
                   }}
                 >
-                  {langs[l]}
+                  {langDict[l]}
                 </div>
               ))}
             </div>
@@ -117,14 +117,14 @@ const ProgressBars = ({ langs }: ProgressBarsProps) => {
 }
 
 export default function Skill({ pageIndex, transDirect, setPage }: ComponentProps): JSX.Element {
-  const langs = useLangs()
+  const langDict = useLangDict()
 
   return (
     <Layout pageIndex={pageIndex} transDirect={transDirect} setPage={setPage} title="skill">
       <TextBox enableMargins={[false, true]} proportion={50}>
         <h2>language</h2>
         <p>bytes in public repositories</p>
-        <ProgressBars langs={langs} />
+        <ProgressBars langDict={langDict} />
       </TextBox>
 
       <TextBox enableMargins={[true, false]} proportion={50}>
