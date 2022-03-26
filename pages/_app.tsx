576fc519d4b1126ec2e0b01b13c100d7c60e38a9
kazumakano <kazumakano@outlook.jp>
2022-03-26 17:27:07 +0900

    several updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,12 +33,6 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         <title>{process.env.NEXT_PUBLIC_TITLE}</title>
         <meta name="description" content={process.env.NEXT_PUBLIC_DESC} />
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossOrigin="anonymous"
-        />
       </Head>
 
       <AnimatePresence initial={false}>
