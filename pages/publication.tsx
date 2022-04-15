2339a60eb6bda2dd6152fd4419e30f347c13b7c1
kazumakano <kazumakano@outlook.jp>
2022-04-16 01:56:47 +0900

    delete publication-table.tsx

--- pages/publication.tsx
+++ pages/publication.tsx
@@ -1,8 +1,40 @@
+import papers from "../public/papers.json"
+import Table from "react-bootstrap/Table"
+import { getFormattedDate } from "../components/datetime"
+import { CopyBtn } from "../components/button"
 import type { ComponentProps } from "./_app"
 import Layout from "../components/layout"
 import TextBox from "../components/text-box"
-import PublicationTable from "../components/publication-table"
 
+
+type PublicationTableProps = {
+  isInternational: boolean
+} 
+
+const PublicationTable = ({ isInternational }: PublicationTableProps) => (
+  <Table hover striped>
+    <thead>
+      <tr>
+        <th>date</th>
+        <th>title</th>
+        <th>conference</th>
+        <th>authorship</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      {(isInternational ? papers.international : papers.domestic).map((p, i) => (
+        <tr key={i}>
+          <td>{getFormattedDate(p.date[1], p.date[0])}</td>
+          <td>{p.title}</td>
+          <td>{p.conference}</td>
+          <td>{p.authorship}</td>
+          <td><CopyBtn text={p.title} /></td>
+        </tr>
+      ))}
+    </tbody>
+  </Table>
+)
 
 export default function Publication({ pageIndex, transDirect, setPage }: ComponentProps): JSX.Element {
   return (
