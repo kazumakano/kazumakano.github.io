56a0c95f255618010f479cdab7a28c3d0950cd85
kazumakano <kazumakano@outlook.jp>
2022-04-16 16:02:18 +0900

    update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,15 +1,32 @@
+import styles from "../styles/Contact.module.css"
 import type { ComponentProps } from "./_app"
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import Layout from "../components/layout"
 import TextBox from "../components/text-box"
-import Link from "next/link"
+import { MailerBtn } from "../components/button"
 
 
 export default function Contact({ setIsInputting, pageIndex, transDirect, setPage }: ComponentProps): JSX.Element {
   const [mailBody, setMailBody] = useState<string>("")
   const [mailSubject, setMailSubject] = useState<string>("")
 
-  const uri = `mailto:${process.env.NEXT_PUBLIC_EMAIL}?subject=${mailSubject}&body=${mailBody}`.replace(/\n/g, "%0d%0a")
+  const getUri = useCallback(() => {
+    let uri = `mailto:${process.env.NEXT_PUBLIC_EMAIL}`
+
+    if (mailBody != "" && mailSubject != "") {
+      uri += `?body=${mailBody}&subject=${mailSubject}`
+    }
+    else if (mailBody != "") {
+      uri += `?body=${mailBody}`
+    }
+    else if (mailSubject != "") {
+      uri += `?subject=${mailSubject}`
+    }
+
+    return uri
+  }, [mailBody, mailSubject])
+
+  const uri = getUri()
 
   return (
     <Layout pageIndex={pageIndex} transDirect={transDirect} setPage={setPage} title="skill">
@@ -19,26 +36,31 @@ export default function Contact({ setIsInputting, pageIndex, transDirect, setPag
 
       <TextBox enableMargins={[true, false]} proportion={50}>
         <h2>mail</h2>
-        <label htmlFor="mail-subject">subject</label>
-        <input
-          id="mail-subject"
-          onBlur={() => setIsInputting(false)}
-          onChange={event => setMailSubject(event.target.value)}
-          onFocus={() => setIsInputting(true)}
-          type="text"
-        />
+        <div className={styles.mailForm}>
+          <div>
+            <label htmlFor="mail-subject">subject</label>
+            <input
+              autoComplete="off"
+              id="mail-subject"
+              onBlur={() => setIsInputting(false)}
+              onChange={event => setMailSubject(event.target.value)}
+              onFocus={() => setIsInputting(true)}
+              type="text"
+            />
+          </div>
 
-        <label htmlFor="mail-body">body</label>
-        <textarea
-          id="mail-body"
-          onBlur={() => setIsInputting(false)}
-          onChange={event => setMailBody(event.target.value)}
-          onFocus={() => setIsInputting(true)}
-        />
+          <div>
+            <label htmlFor="mail-body">body</label>
+            <textarea
+              id="mail-body"
+              onBlur={() => setIsInputting(false)}
+              onChange={event => setMailBody(event.target.value.replace(/\n/g, "%0d%0a"))}
+              onFocus={() => setIsInputting(true)}
+            />
+          </div>
 
-        <Link href={uri}>
-          <input type="submit" />
-        </Link>
+          <MailerBtn uri={uri} />
+        </div>
       </TextBox>
     </Layout>
   )
