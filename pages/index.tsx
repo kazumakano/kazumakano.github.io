2b2b17f5c6a4347cc1debf463d31bfc32800cc05
kazumakano <kazumakano@outlook.jp>
2022-04-29 15:25:33 +0900

    update index.ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,12 @@ const LinkedTd = ({ children, url }: LinkedTdProps) => (
   </td>
 )
 
+const rightIcon = (
+  <svg height="1em" viewBox="0 0 16 16" width="1em">
+    <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+  </svg>
+)
+
 export default function Home({ pageIndex, transDirect, setPage }: ComponentProps): JSX.Element {
   const [hintMsg, setHintMsg] = useState<JSX.Element>(<></>)
 
@@ -33,7 +39,7 @@ export default function Home({ pageIndex, transDirect, setPage }: ComponentProps
     window.setTimeout(setHintMsg, HINT_DELAY, (
       <div className={styles.hintMsg}>
         <p>
-          press <kbd>Enter</kbd> or swipe
+          press <kbd>Enter</kbd> or swipe {rightIcon}
         </p>
       </div>
     ))
