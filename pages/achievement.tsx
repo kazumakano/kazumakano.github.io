e3564b26c4345efd3a14352354fffcc427923dd1
kazumakano <kazumakano@outlook.jp>
2023-10-02 18:11:10 +0900

    add xDR URL

--- pages/achievement.tsx
+++ pages/achievement.tsx
@@ -1,5 +1,6 @@
 import styles from "../styles/Achievement.module.css"
 import achieves from "../public/achievements.json"
+import type { ReactNode } from "react"
 import type { ComponentProps } from "./_app"
 import Layout from "../components/layout"
 import TextBox from "../components/text-box"
@@ -8,35 +9,46 @@ import { getFormattedDate } from "../hooks/datetime"
 import Link from "next/link"
 
 
+type LinkedTdProps = {
+  children: ReactNode
+  url: string
+}
+
+const LinkedTd = ({ children, url }: LinkedTdProps) => (
+  <td>
+    <Link href={url}>
+      <a target="_blank">
+        {children}
+      </a>
+    </Link>
+  </td>
+)
+
 export default function Achievement({ pageIndex, transDirect, setTransDirect }: ComponentProps): JSX.Element {
   return (
     <Layout pageIndex={pageIndex} transDirect={transDirect} setTransDirect={setTransDirect} title="achievement">
       <TextBox enableMargins={[false, false]} proportion={100}>
         <h2>award</h2>
-        <Table borderless>
+        <Table borderless className={styles.achieveTable}>
           <tbody>
             {achieves.award.map((a, i) => (
               <tr key={i}>
                 <td>{getFormattedDate(a.date[1], a.date[0])}</td>
-                <td>{a.title}</td>
+                {a.url ? <LinkedTd url={a.url}>{a.title}</LinkedTd> : <td>{a.title}</td>}
               </tr>
             ))}
           </tbody>
         </Table>
 
         <h2>qualification</h2>
-        <Table borderless className={styles.qualTable}>
+        <Table borderless className={styles.achieveTable}>
           <tbody>
             {achieves.qualification.map((q, i) => (
               <tr key={i}>
                 <td>{getFormattedDate(q.date[1], q.date[0])}</td>
-                <td>
-                  <Link href={q.url}>
-                    <a target="_blank">
-                      {q.title}
-                    </a>
-                  </Link>
-                </td>
+                <LinkedTd url={q.url}>
+                  {q.title}
+                </LinkedTd>
               </tr>
             ))}
           </tbody>
