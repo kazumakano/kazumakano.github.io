8265b24500f6254745393aa1f68b9fe981dd3a93
kazumakano <kazumakano@outlook.jp>
2026-05-02 20:02:44 +0900

    build

--- docs/_next/static/chunks/0zriiihtgnvzm.js
+++
@@ -1,9 +0,0 @@
-__turbopack_load_page_chunks__("/skill", [
-  "static/chunks/0w0v-0~rlsbj~.js",
-  "static/chunks/16ylyuct0683q.js",
-  "static/chunks/0~xzcxwplrd-0.js",
-  "static/chunks/0ggzwg36f~~bz.js",
-  "static/chunks/0f95o6axllt2t.js",
-  "static/chunks/04r1ff~jzz7eh.css",
-  "static/chunks/turbopack-0e-n-ukov03v3.js"
-])
--- docs/_next/static/chunks/180vzb4zjw5lu.js
+++
@@ -1,7 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/12~..g30bwnu9.js",
-  "static/chunks/0~xzcxwplrd-0.js",
-  "static/chunks/16ylyuct0683q.js",
-  "static/chunks/04r1ff~jzz7eh.css",
-  "static/chunks/turbopack-0h0.s4vl6lqc9.js"
-])
